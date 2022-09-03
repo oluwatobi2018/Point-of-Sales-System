1ad6a054560f0a5a77bc96786bd202d56520cd67
Christopher Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2022-09-03 18:41:00 +0100

    Update pos.java

--- pos.java
+++ pos.java
@@ -55,17 +55,3 @@ public class MainApp extends Application {
     }
 
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
